7c654871f613885bd651413c3e2166ad8829122f
Haneul Kim <ttcielott@gmail.com>
2023-04-20 12:46:56 +0100

    added the function, split_size_as_column, split_unitprice_as_column

--- main_files/functions_main.py
+++ main_files/functions_main.py
@@ -33,11 +33,11 @@ def extract_without(file_path, column_numbers):
 
     return list_of_new_data_list
 
-def split_into_order(list_of_data_list, column_number):
+def split_into_order(list_of_data_list, column_number, sep=','):
     list_of_new_data_list =[]
     for data in list_of_data_list:
         updating_col = data[column_number]
-        orders_list = updating_col.split(",")
+        orders_list = updating_col.split(sep)
 
         if len(orders_list) == 1:
             list_of_new_data_list.append(data)
@@ -49,8 +49,50 @@ def split_into_order(list_of_data_list, column_number):
 
     return list_of_new_data_list
 
+def split_size_as_column(list_of_data_list, column_number, sep=' '):
+    list_of_new_data_list =[]
+    for data in list_of_data_list:
+        updating_col = data[column_number]
+        striped_string = updating_col.strip()
+        print(striped_string)
+        split_list= striped_string.split(sep, 1)
+
+        if len(split_list) == 1:
+            raise Exception('No split occurred')
+        elif len(split_list) > 1:
+            
+            original_data = data.copy()
+            original_data[column_number] = split_list[0]
+            original_data.append(split_list[1])
+            list_of_new_data_list.append(original_data)
+
+    return list_of_new_data_list
+
+def split_unitprice_as_column(list_of_data_list, column_number, sep=' - '):
+    list_of_new_data_list =[]
+    for data in list_of_data_list:
+        updating_col = data[column_number]
+        split_list = updating_col.strip().rsplit(sep, 1)
+
+        if len(split_list) == 1:
+            raise Exception('No split occurred')
+        elif len(split_list) > 1:
+            
+            original_data = data.copy()
+            original_data[column_number] = split_list[0]
+            original_data.append(split_list[1])
+            list_of_new_data_list.append(original_data)
+
+    return list_of_new_data_list
         
 if __name__ == "__main__":
     new_data = extract_without('../data/chesterfield_25-08-2021_09-00-00.csv', [6])
 
-    print(split_into_order(new_data, 3))
+    new_data = split_into_order(new_data, 3)
+
+    new_data = split_size_as_column(new_data[3:10], 3)
+
+    new_data = split_unitprice_as_column(new_data[3:10], 6)
+
+    for ele in new_data:
+        print(ele)
