422293db1b7788e8d03743bbce865928cd502e03
Haneul Kim <ttcielott@gmail.com>
2023-04-25 19:57:05 +0100

    fixed conditionals error

--- init.sh
+++ init.sh
@@ -5,23 +5,26 @@ echo -n "What is your operation system? [W if Window, L if Linux]: "
 read var
 
 # create a docker container for database
-cd database
-docker compose up -d
+# cd database
+# docker compose up -d
 
-# cerate a virtual environment
-if [[ $var == 'W']]
+if [[ $var == 'W' ]]
+then
+    # cerate a virtual environment
     py -3.10 -m venv .venv
 
     # activate the virtual environment
     .venv/Scripts/Activate.bat
-else
-    python3.10 -m venv .venv
-    source .venv/bin/activate
+elif [[ $var == 'L' ]]
 then
-    echo "a virtural environment was created and activated"
+    # cerate a virtual environment
+    python3.10 -m venv .venv
+    # activate the virtual environment
+    source .venv/bin/activate
+
+echo "a virtural environment was created and activated"
 fi
     
 # install packages from requirements.txt
 cd main_files
-pip install -r requirements.txt
-
+python -m pip install -r requirements.txt
