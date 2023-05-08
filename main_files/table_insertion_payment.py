5b922b3907ccdd8d74b41c15db326e805bda672b
Haneul Kim <ttcielott@gmail.com>
2023-05-08 21:16:49 +0100

    added the code to check if the payment method name already exisits in the database.

--- main_files/table_insertion_payment.py
+++ main_files/table_insertion_payment.py
@@ -1,34 +1,51 @@
 # Import db connection previously created
 import csv
-import os
 from main_files.databaseconn_main import *
 from main_files.functions_main import *
 
 # Simply function to insert payment options in payments table
 
-def insert_payment (conn, cursor, payment_method_id:int, payment_method_name:str):
+def insert_payment (conn, cursor, payment_method_name: str):
     '''
     This function reads files in the data folder and then returns 
     the unique payment methods stored in each csv file, if a different
     method is found then it will add it to the db table 
     '''
-    cursor.execute("INSERT INTO payments (payment_method_id, payment_method_name) VALUES (%s, %s)", (payment_method_id, payment_method_name))
-    conn.commit()
-    print('Rows Added')
+    
+    sql = '''SELECT COUNT(*)
+             FROM payments
+             WHERE payment_method_name = %s
+             '''
+    data_values = (payment_method_name,)
+
+    cursor.execute(sql, data_values)
+    existing_payment_method = cursor.fetchone()[0]
+
+    if existing_payment_method :
+        print(f"The payment method, {payment_method_name} already exists in the database.")
+
+    else:
+        sql = '''INSERT INTO payments 
+                (payment_method_name)
+                VALUES (%s)
+                '''
+        data_values = (payment_method_name,)
+        cursor.execute(sql, data_values)
+        conn.commit()
+        print(f'New row with {payment_method_name} was inserted.')
 
 csv_files = get_csv_files_path() 
 
 unique_payment_names = set()
 
-for csv_file in csv_files:
-    with open(csv_file, "r") as f:
-        csv_reader = csv.reader(f)
+for file_path in csv_files:
+    csv_reader = read_csv(file_path)
 
-        for row in csv_reader:
-            payment_method_name = row[5]
+    for row in csv_reader:
+        payment_method_name = row[5]
 
-            # Add the payment name to the set of unique payment names
-            unique_payment_names.add(payment_method_name)
+        # Add the payment name to the set of unique payment names
+        unique_payment_names.add(payment_method_name)
 
 # Convert the set of unique branch names to a list
 payment_method_names = list(unique_payment_names)
@@ -36,12 +53,10 @@ payment_method_names = list(unique_payment_names)
 # Sort the list of branch names alphabetically
 payment_method_names.sort()
 
-new_rows = [(i+1, payment_method_names[i]) for i in range(len(payment_method_names))]
-
 conn, cursor = database_connection(dbname, user, password, host, port)
 
-for row in new_rows:
-    insert_payment(conn, cursor, row[0], row[1])
+for payment_method_name in payment_method_names:
+    insert_payment(conn, cursor, payment_method_name)
 
 cursor.close()
 conn.close()
