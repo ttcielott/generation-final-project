72f828b9079bdbb130b4d0d01556d685808ff085
Haneul Kim <ttcielott@gmail.com>
2023-05-08 22:05:38 +0100

    added the code that checks if the product information already exists in databse

--- main_files/table_insertion_product.py
+++ main_files/table_insertion_product.py
@@ -1,20 +1,17 @@
-import os
-from dotenv import load_dotenv
 from main_files.databaseconn_main import *
 from main_files.functions_main import transform_branch_file
 from typing import List
 from main_files.functions_main import *
 
 
-# create a database connnection
-conn, cursor = database_connection(dbname, user, password, host, port)
 
 csv_files = get_csv_files_path()
-    
+
 # apply all transformation functions in order and combine them into one list
 transformed_branch_data =[]
 for filepath in csv_files:
-    transformed_branch_data += transform_branch_file(filepath)
+      csv_list = read_csv(filepath)
+      transformed_branch_data += transform_branch_file(csv_list)
 
 
 def get_unique_product_n_price(list_of_data_list: List):
@@ -35,7 +32,7 @@ def get_unique_product_n_price(list_of_data_list: List):
 
             # select product name, product size, product price
             # save them in a list
-            product_row = [row[-2], row[2], row[-1]]
+            product_row = [row[4], row[5], row[6]]
 
             # append product_row to product_rows
             product_rows.append(product_row)
@@ -50,23 +47,47 @@ def get_unique_product_n_price(list_of_data_list: List):
       return unique_product_info
 
 
-def load_product(conn, cursor, product_info, tablename):
+def load_product(conn, cursor, product_info):
+      sql = '''SELECT COUNT(*)
+             FROM products
+             WHERE product_name = %s
+             AND product_size = %s
+             AND product_price = %s;
+             '''
       
-      sql = f"""
-            INSERT INTO {tablename}(product_name, product_size, product_price)
-            VALUES (%s, %s, %s);
-            """
-      
-      for row in product_info:
-            
-            data_values = tuple(row)
+      data_values = tuple(product_info)
+      cursor.execute(sql, data_values)
+      existing_product_info = cursor.fetchone()[0]
+
+      if existing_product_info :
+            print(f'''The product information,
+                  [product_name: {product_info[0]}, 
+                  product_size: {product_info[1]}, 
+                  product_price: {product_info[2]}] already exists in the database.''')
+
+      else:
+            sql = f"""
+                  INSERT INTO products(product_name, product_size, product_price)
+                  VALUES (%s, %s, %s);
+                  """
             cursor.execute(sql, data_values)
-      conn.commit()
-      cursor.close()
-      conn.close()
+            conn.commit()
+      
+            print(f'''New row with the product information,
+                  [product_name: {product_info[0]}, 
+                  product_size: {product_info[1]}, 
+                  product_price: {product_info[2]}] was inserted.''')
+
 
 if __name__ == '__main__':
-      product_info = get_unique_product_n_price(transformed_branch_data)
-      load_product(conn, cursor, product_info, 'products')
+      product_info_list = get_unique_product_n_price(transformed_branch_data)
 
+      # create a database connnection
+      conn, cursor = database_connection(dbname, user, password, host, port)
+
+      for product_info in product_info_list:
+            load_product(conn, cursor, product_info)
+
+      cursor.close()
+      conn.close()
 
