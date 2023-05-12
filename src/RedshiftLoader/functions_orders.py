22d3adf83e29f830362ed3524a1efa1fe68363bb
Haneul Kim <ttcielott@gmail.com>
2023-05-12 17:52:41 +0100

    changed the variable names

--- src/RedshiftLoader/functions_orders.py
+++ src/RedshiftLoader/functions_orders.py
@@ -11,9 +11,9 @@ def convert_to_list_of_dictionary(list_of_data: List, key_names: List):
             [1, '25/04/2021', '23:10', 'Mochafield', 'Flat white', 'Regular', 2.15, 12.95,'CASH']]
     Return:
         e.g.
-            [{'temp_transaction_id': 1, 'order_data': '25/04/2021', 'order_time': '23:10', 'branch_name': 'Mochafield', 
+            [{'temp_transaction_id': 1, 'order_data': '25/04/2021', 'transaction_time': '23:10', 'branch_name': 'Mochafield', 
             'product_name': 'Flavoured iced latte - Caramel', 'product_size': 'Large', 'product_price' : 3.25, 'total_amount': 12.95, 'payment_method_id': 'CASH'},
-            {'temp_transaction_id': 1, 'order_data': '25/04/2021', 'order_time': '23:10', 'branch_name': 'Mochafield', 
+            {'temp_transaction_id': 1, 'order_data': '25/04/2021', 'transaction_time': '23:10', 'branch_name': 'Mochafield', 
             'product_name': 'Flat white', 'product_size': 'Regular', 'product_price' : 2.15, 'total_amount': 12.95, 'payment_method_id': 'CASH'}]
     
     """
@@ -27,7 +27,7 @@ def convert_to_list_of_dictionary(list_of_data: List, key_names: List):
 
 def collape_same_transactions_into_one(order_dict_list: List):
     """
-    group product_name, product_size, order_qty by temp_transaction_id 
+    Group product_name, product_size, order_qty by temp_transaction_id 
     and make them in a list under 'orders_list' key.
 
     Args:
@@ -36,7 +36,7 @@ def collape_same_transactions_into_one(order_dict_list: List):
             [
                 {'temp_transaction_id': 1, 
                 'order_data': '25/04/2021', 
-                'order_time': '23:10', 
+                'transaction_time': '23:10', 
                 'branch_name': 'Mochafield', 
                 'product_name': 'Flavoured iced latte - Caramel', 'product_size': 'Large', 'product_price' : 3.25, 
                 'total_amount': 12.95, 
@@ -44,7 +44,7 @@ def collape_same_transactions_into_one(order_dict_list: List):
             
                 {'temp_transaction_id': 1, 
                 'order_data': '25/04/2021', 
-                'order_time': '23:10', 
+                'transaction_time': '23:10', 
                 'branch_name': 'Mochafield', 
                 'product_name': 'Flat white', 'product_size': 'Regular', 'product_price' : 2.15, 
                 'total_amount': 12.95, 
@@ -55,7 +55,7 @@ def collape_same_transactions_into_one(order_dict_list: List):
             [
                 {'temp_transaction_id': 1, 
                 'order_data': '25/04/2021', 
-                'order_time': '23:10', 
+                'transaction_time': '23:10', 
                 'branch_name': 'Mochafield', 
                 'orders_list': [
                                     {'product_name': 'Flavoured iced latte - Caramel', 'product_size': 'Large', 'product_price' : 3.25}, 
@@ -82,3 +82,43 @@ def collape_same_transactions_into_one(order_dict_list: List):
             new_order_dict_list.append(order_dict)
 
     return new_order_dict_list
+
+def get_unique_orders_dictinaries(record_index: int, from_path: str, list_of_data: List):
+    """
+    Convert a list of lists of data into a list of dictionaries wth key names.
+    Group product_name, product_size, and order_qty by temp_transaction_id 
+    and make them in a list under 'orders_list' key.
+    Args:
+        list_of_data: list, a list of list of each order
+        key_names: list, a list of key names
+        e.g. 
+            [[1, '25/04/2021', '23:10', 'Mochafield', 'Flavoured iced latte - Caramel', 'Large', 3.25, 12.95,'CASH'],
+            [1, '25/04/2021', '23:10', 'Mochafield', 'Flat white', 'Regular', 2.15, 12.95,'CASH']]
+    Return:
+        e.g.
+            [
+                {'temp_transaction_id': 1, 
+                'order_data': '25/04/2021', 
+                'transaction_time': '23:10', 
+                'branch_name': 'Mochafield', 
+                'orders_list': [
+                                    {'product_name': 'Flavoured iced latte - Caramel', 'product_size': 'Large', 'product_price' : 3.25}, 
+                                    {'product_name': 'Flat white', 'product_size': 'Regular', 'product_price' : 2.15,}
+                                ]
+                'total_amount': 12.95, 'payment_method_id': 'CASH'}
+            ]
+    
+    """
+    
+    # set key names
+    key_names = ['temp_transaction_id', 'transaction_date', 'transaction_time', 'branch_name', 
+                    'product_name', 'product_size', 'product_price', 'order_qty', 
+                    'total_amount', 'payment_method']
+
+    # convert list into diction with column names
+    order_dict_list = convert_to_list_of_dictionary(list_of_data, key_names)
+
+    # collapse the same transactions into one list
+    unique_order_dict_list = collape_same_transactions_into_one(order_dict_list)
+
+    return unique_order_dict_list
