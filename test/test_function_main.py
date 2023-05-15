7abc2a47e0e956082ff53a3dc8c153185f358c3b
Rishi <rishiram654@hotmail.com>
2023-05-15 11:10:43 +0100

    fixed the module referencing issue

--- test/test_function_main.py
+++ test/test_function_main.py
@@ -1,6 +1,6 @@
 import csv
 import unittest
-from src..functions_transformation import *
+from src.CSVTransformer.functions_transformation import *
 
 # load_dotenv('database/.env')  # load environment variables from .env file
 
