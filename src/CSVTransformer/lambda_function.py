2c58c24ba9f204fd73220729ffaf2d8ec014b06e
Sam Feighery <sfeighery@gmail.com>
2023-05-17 16:41:27 +0100

    Updating lambda for more logging

--- src/CSVTransformer/lambda_function.py
+++ src/CSVTransformer/lambda_function.py
@@ -10,14 +10,14 @@ def lambda_handler(event, context):
     print(f'lambda_handler started event = {event}')
     try:
         for msg_no, msg in enumerate(event['Records']):
-            print(f'lambda_handler: msg_no = {msg_no} and msg = {msg}')
+            print(f'lambda_handler: msg_no = {msg_no}')
             message_body = msg['body']
             msg_body_json = json.loads(message_body)
-            print(f'lambda_handler: msg_body_json = {msg_body_json}')
+            print(f'lambda_handler: msg_body_json loaded okay')
             bucket = msg_body_json['Records'][0]['s3']['bucket']['name']
             key = msg_body_json['Records'][0]['s3']['object']['key']
             print(f'lambda_handler: bucket = {bucket} and key = {key}')
-
+    
             
             response = s3.get_object(Bucket=bucket, Key=key)
             print(f'lambda_handler got s3 response for key = {key}')
@@ -31,7 +31,7 @@ def lambda_handler(event, context):
             #changing the key to only return the name of the store
             store = key.split('/')[-1].split('_')[0]
             print(f'lambda_handler found store = {store} for key = {key}')
-
+    
             # Transform the data
             transformed_data = transform_branch_file(data)
             print(f'lambda_handler transform_data rows = {len(transformed_data)} for file {store}')
@@ -44,27 +44,30 @@ def lambda_handler(event, context):
             writer.writerows(transformed_data)
             
             # Store the transformed csv file in a new bucket
-            s3.put_object(Body=output.getvalue(), Bucket='mocha-madness-transformed-data-v2', Key=key)
-            print(f'lambda_handler finished processing file {key}')
+            tranformed_bucket = 'mocha-madness-transformed-data-v2'
+            s3.put_object(Body=output.getvalue(), Bucket= tranformed_bucket, Key=key)
+            print(f'lambda_handler saved file key = {key} in bucket = {tranformed_bucket}')
             
             # Create SQS client
             sqs = boto3.client('sqs')
-
-            queue_url = 'https://sqs.eu-west-1.amazonaws.com/015206308301/mocha-madness-redshift-loader-queue'
-            message_json = {'bucket': bucket, 'key': key}
+            queue_name = 'mocha-madness-redshift-loader-queue'
+            queue_url = 'https://sqs.eu-west-1.amazonaws.com/015206308301/'+queue_name
+            message_json = {'bucket': tranformed_bucket, 'key': key}
             # covert into json string
             message_text = json.dumps(message_json)
+            
+            print(f'lambda_handler sending message to {queue_name} for key = {key}')
             
             # Send message to SQS queue
             response = sqs.send_message(
                 QueueUrl= queue_url,
                 DelaySeconds=10,
-
+    
                 MessageBody=(message_text)
             )
-
+            print(f'lambda_handler sent message to {queue_name} for key = {key}')
+    
             print(f'lambda_handler finished for key = {key}')
-            
+    
     except Exception as e:
         print(f'Error in lambda_handler = ${e}')
-    
