d5d550914ae1ec03342330ba37c7d8d59409720d
Haneul Kim <ttcielott@gmail.com>
2023-05-17 22:14:06 +0100

    fixed indentation

--- src/RedshiftLoader/lambda_function.py
+++ src/RedshiftLoader/lambda_function.py
@@ -23,40 +23,38 @@ def lambda_handler(event, context):
             key = msg_body_json['Records'][0]['s3']['object']['key']
             print(f'lambda_handler: bucket = {bucket} and key = {key}')
                     
-        from_path = f's3://{bucket}/{key}'
-        print(f'lambda_handler msg_no= {msg_no} from path {from_path}')
-        
-        
-        # access the conent of new data
-        response = s3.get_object(Bucket=bucket, Key=key)
-        print(f'lambda_handler s3 response = {response}')
-        content = response['Body'].read().decode('utf-8')
-        print(f'lambda_handler s3 content = {content}')
+            from_path = f's3://{bucket}/{key}'
+            print(f'lambda_handler msg_no= {msg_no} from path {from_path}')
+            
+            
+            # access the conent of new data
+            response = s3.get_object(Bucket=bucket, Key=key)
+            content = response['Body'].read().decode('utf-8')
 
-        # read the csv file of new data
-        data = []
-        reader = csv.reader(StringIO(content))
-        for row in reader:
-                data.append(row)
-        print(f'lambda_handler loaded {len(data)} rows from file {key}')
-        
-        # table, products
-        load_to_table_products(msg_no, from_path, data)
-        print(f'lambda_handler msg_no = {msg_no} from path {from_path}, the function, load_to_table_products is completed')
+            # read the csv file of new data
+            data = []
+            reader = csv.reader(StringIO(content))
+            for row in reader:
+                    data.append(row)
+            print(f'lambda_handler loaded {len(data)} rows from file {from_path}')
+            
+            # table, products
+            load_to_table_products(msg_no, from_path, data)
+            print(f'lambda_handler from path {from_path}, the function, load_to_table_products is completed')
 
 
-        # table, branches
-        load_to_table_branches(msg_no, from_path, data)
-        print(f'lambda_handler msg_no = {msg_no} from path {from_path}, the function, load_to_table_branches is completed')
+            # table, branches
+            load_to_table_branches(msg_no, from_path, data)
+            print(f'lambda_handler from path {from_path}, the function, load_to_table_branches is completed')
 
-        # table, payments
-        load_to_table_payments(msg_no, from_path, data)
-        print(f'lambda_handler msg_no = {msg_no} from path {from_path}, the function, load_to_table_payments is completed')
+            # table, payments
+            load_to_table_payments(msg_no, from_path, data)
+            print(f'lambda_handler from path {from_path}, the function, load_to_table_payments is completed')
 
-        # table, transactions & table, orders
-        load_to_table_transactions_n_orders(msg_no, from_path, data)
-        print(f'lambda_handler msg_no = {msg_no} from path {from_path}, the function, load_to_table_transactions_n_order_produc is completed')
-        
-        return 'test is done.'
+            # table, transactions & table, orders
+            load_to_table_transactions_n_orders(msg_no, from_path, data)
+            print(f'lambda_handler from path {from_path}, the function, load_to_table_transactions_n_orders is completed')
+            
+            print(f'lambda_handler finished for from_path = {from_path}')
     except Exception as e:
         print(f'Error in lambda_handler = {e}')
